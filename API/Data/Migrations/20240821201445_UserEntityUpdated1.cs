e96ef3531488fe0fbe93f923485c952e6eed81f2
MartinCastillo99 <martin.castillo@greenlight.com.py>
2024-08-21 16:14:59 -0400

    Commit

---
+++ API/Data/Migrations/20240821201445_UserEntityUpdated1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserEntityUpdated1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PsswordSalt",
+                table: "Users",
+                newName: "PasswordSalt");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordSalt",
+                table: "Users",
+                newName: "PsswordSalt");
+        }
+    }
+}
