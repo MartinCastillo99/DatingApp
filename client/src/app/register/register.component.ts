d1c2e082a68fba205c429cc7f4c1debc042a6662
MartinCastillo99 <martin.castillo@greenlight.com.py>
2024-08-27 14:06:45 -0400

    Commit

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  model: any = {}
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+}
