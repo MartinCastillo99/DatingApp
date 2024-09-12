15abf633f3c8ed1d7c31152ee73cdfc16a71a284
MartinCastillo99 <martin.castillo@greenlight.com.py>
2024-09-12 14:54:30 -0400

    Section 7

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net;
+using API.Errors;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger,
+IHostEnvironment env)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = env.IsDevelopment()
+            ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
+            ? new ApiException(context.Response.StatusCode, ex.Message, "Internat server error");
+        }
+    }
+}
