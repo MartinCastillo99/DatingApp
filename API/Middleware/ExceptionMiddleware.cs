5a71b6708bf88f86f0717f39d5c22917e883742c
MartinCastillo99 <martin.castillo@greenlight.com.py>
2024-09-12 16:01:27 -0400

    End of section 7

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Text.Json;
 using API.Errors;
 
 namespace API.Middleware;
@@ -21,7 +22,15 @@ IHostEnvironment env)
 
             var response = env.IsDevelopment()
             ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace)
-            ? new ApiException(context.Response.StatusCode, ex.Message, "Internat server error");
+            : new ApiException(context.Response.StatusCode, ex.Message, "Internat server error");
+
+            var options = new JsonSerializerOptions{
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var json = JsonSerializer.Serialize(response, options);
+
+            await context.Response.WriteAsync(json);
         }
     }
 }
