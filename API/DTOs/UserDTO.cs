405e055017d59dc73897266114aada3ff7a62d02
MartinCastillo99 <martin.castillo@greenlight.com.py>
2024-10-02 14:49:33 -0400

    End of section 11

--- API/DTOs/UserDTO.cs
+++ API/DTOs/UserDTO.cs
@@ -4,7 +4,7 @@ namespace API.DTOs;
 
 public class UserDTO
 {
-public required string Username { get; set; }
-
-public required string Token { get; set; }
+    public required string Username { get; set; }
+    public required string Token { get; set; }
+    public string? PhotoUrl { get; set; }
 }
